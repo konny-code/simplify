d118de51cedc48e44cb008e22000069542d959d3
Tasuku Koni <koni.tasuku@gmail.com>
2025-11-18 14:42:50 +0900

    worked on articles #show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,8 @@
 class ArticlesController < ApplicationController
+  def show
+    @article = Article.find(params[:id])
+  end
 end
+
+#since we are only showing the simplified article
+#however, we are going to store the
