44cca7ce8b8c8d9aa899d923ba35a6080fda1236
yume towhida <towhida.h.yume@gmail.com>
2025-11-19 14:30:00 +0900

    interim commit multiple cleanups

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,27 +7,18 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
-  
+
+
   def create
     @article = Article.new(article_params)
     @article.user = current_user
     if @article.save
-      @article.generate_content
       redirect_to article_path(@article)
     else
       render "pages/home", status: :unprocessable_entity
     end
   end
 
-  def update
-    @article = Article.find(params[:id])
-    if @article.update(article_params)
-       @article.generate_content
-       redirect_to article_path(@article)
-    else
-      render :show, status: :unprocessable_entity
-    end
-  end
 
   private
 
