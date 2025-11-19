9758e76d42e10729b9320a92c9ac12ce96ec53b9
Felipe Silva <talk.felipe@gmail.com>
2025-11-19 17:57:15 +0900

    added user first last name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,9 +10,9 @@ class ApplicationController < ActionController::Base
   # Extra params for sign up / account update
   def configure_permitted_parameters
     # For app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:language_level])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:language_level, :first_name, :last_name])
 
     # For app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:language_level])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:language_level, :first_name, :last_name])
   end
 end
